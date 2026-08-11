578618b68a8de2c13354ef15cc18b7b0b461e3a0
wrysha <201246353+wrysha@users.noreply.github.com>
2026-08-11 23:00:26 +0800

    Update LavaLust landing page

--- app/views/welcome_page.php
+++ app/views/welcome_page.php
@@ -6,7 +6,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to LavaLust</title>
+    <title>Welcome to LavaLust - Lab 2 by Wrysha</title>
     <link rel="shortcut icon" href="data:image/x-icon;," type="image/x-icon">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;500;600;700;800&family=Unbounded:wght@400;500&display=swap" rel="stylesheet">
